e8f95fd538b6be0f5f82f2e8792a864b27df4214
korchel <korchel21@gmail.com>
2024-10-15 16:38:00 +0300

    todo relation added

--- db.js
+++ db.js
@@ -12,9 +12,16 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   password: process.env.DATABASE_PASSWORD,
   user: process.env.DATABASE_USER,
-
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+const createTable = async () => {
+  await Pool.query(`CREATE TABLE IF NOT EXISTS todo 
+  (id serial PRIMARY KEY, task VARCHAR (255), 
+  done BOOLEAN;`);
+};
+
+createTable();
 module.exports = pool;
