1e90a053eb2386265607f9f4667c78358426d494
korchel <korchel21@gmail.com>
2024-10-15 15:18:40 +0300

    psql pool changed

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
-const Pool = require('pg').Pool;
-
-const pool = new Pool({
-    user: '',
-    host: 'localhost',
-    database: 'postgres',
-    password: '',
-    port: 5432,
-});
+const Pool = require("pg").Pool;
 
 // const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-// })
+//     user: '',
+//     host: 'localhost',
+//     database: 'postgres',
+//     password: '',
+//     port: 5432,
+// });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  password: process.env.DATABASE_PASSWORD,
+  user: process.env.DATABASE_USER,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 module.exports = pool;
