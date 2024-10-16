00672eedb6839f606d2a0c1e37936c389eaf9923
korchel <korchel21@gmail.com>
2024-10-16 09:45:55 +0300

    pool edited

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
-const Pool = require('pg').Pool;
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'todos',
-  password: 'password',
-  port: 5432,
-});
+const Pool = require("pg").Pool;
 
 // const pool = new Pool({
-//   host: process.env.DATABASE_URL || 'localhost',
-//   password: process.env.DATABASE_PASSWORD || 'password',
-//   user: process.env.DATABASE_USER || 'postgres',
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'todos',
+//   password: 'password',
+//   port: 5432,
 // });
+
+const pool = new Pool({
+  host: process.env.DATABASE_URL || "localhost",
+  password: process.env.DATABASE_PASSWORD || "password",
+  user: process.env.DATABASE_USER || "postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = pool;
